277c9c1cce334b0cf9d4d8434757c7ecd257dcc4
anlici <2115693946@qq.com>
2024-11-19 19:30:37 +0800

    delete

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -10,18 +10,29 @@ const db = low(adapter);
 //导入shortid 生成唯一id
 const shortid = require('shortid');
 /* 记账本列表 */
-router.get('/account/list', function(req, res, next) {
+router.get('/account', function(req, res, next) {
   //list.ejs 文件渲染到页面
-  res.render('list');
+  let accounts = db.get('accounts').value();
+  res.render('list',{accounts:accounts});
 });
 
 // 添加记录
 router.get('/account/create', function(req, res, next) {
-  let accounts = db.get('accounts').value();
- 
-  res.render('create',{accounts:accounts});
+  
+  res.render('create');
 
 });
+
+// 删除
+router.get('/account/:id',function(req,res) {
+  // 获取id
+  let id = req.params.id;
+  // 根据id删除
+  db.get('accounts').remove({id:id}).write();
+  // 成功提醒
+  res.render('success',{msg:'删除成功~~~~~',url:'/account'});
+})
+
 //新增
 router.post('/account',function(req,res) {
   //获取请求体数据 因为app.js中已经配置了body-parser 所以可以直接获取
@@ -31,7 +42,6 @@ router.post('/account',function(req,res) {
   // 向数据库中添加数据 ，...扩展展开
   db.get('accounts').push({id:id,...req.body}).write();
   // 成功提醒
-
   res.render('success',{msg:'添加成功~~~~~',url:'/account'});
   //res.render('create');
 }

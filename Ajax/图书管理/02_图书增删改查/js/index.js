0a6229c465b7f8a6fb4d7c6dca8d6be232d83fb3
anlici <2115693946@qq.com>
2024-11-29 20:45:56 +0800

    图片上传

--- Ajax/图书管理/02_图书增删改查/js/index.js
+++ Ajax/图书管理/02_图书增删改查/js/index.js
@@ -27,7 +27,7 @@ function getBooksList() {
                     <td>${ item.author }</td>
                     <td>${ item.publisher }</td>
                     <td data-id=${item.id}>
-                        <span clas="del">删除</span>
+                        <span class="del">删除</span>
                         <span class="edit">编辑</span>
                     </td>
             </tr>`
@@ -82,14 +82,99 @@ document.querySelector('.add-btn').addEventListener('click',() => {
  */
 // 3.1 事件委托
 document.querySelector('.list').addEventListener('click',(e) => {
-    // 触发时间目标元素
+    // 触发事件目标元素
     const target = e.target;
     // 判断点击是删除元素
-    if(target.classList.container('del')) {
+    if(target.classList.contains('del')) {
         // 自定义属性id，绑定在父级del
         const theId = target.parentNode.dataset.id;
-        console.log(theId);
+        // console.log(theId);
+        
+        // 3.2调用删除接口，告诉后台,并且刷新图书列表
+        axios({
+            url:`http://hmajax.itheima.net/api/books/${theId}`,
+            method:'DELETE',
+        }).then(() => {
+            getBooksList(); 
+        })
+
+        
     }
 
 
+})
+
+/*   4：编辑图书
+   - 4.1 编辑弹框 
+   - 4.2 获取当前编辑图书数据 -> 回显到编辑表单
+   - 4.3 提交保存修改，并且刷新列表
+*/
+
+/* 动态创建edit事件，事件点击
+编辑元素 -> 点击 -> 弹框显示
+修改有潜在任务，收集提交+隐藏，用js控制，不用属性
+*/
+// 4.1 编辑弹框->显示和隐藏
+const editDom = document.querySelector('.edit-modal')
+const editModal = new bootstrap.Modal(editDom)
+// 编辑元素->点击->弹框显示
+document.querySelector('.list').addEventListener('click', e => {
+  // 判断点击的是否为编辑元素
+  if (e.target.classList.contains('edit')) {
+    // 4.2 获取当前编辑图书数据->回显到编辑表单中
+    //dataset.id: 获取该父节点的 data-id 属性值，即书籍的 ID
+    const theId = e.target.parentNode.dataset.id
+    axios({
+        // 路径传参
+        url: `http://hmajax.itheima.net/api/books/${theId}`
+    }).then(res => {
+        const bookObj = res.data.data;
+        // console.log(bookObj);
+        
+        /*  document.querySelcetor('.edit-form .bookname').value = bookObj.bookname;
+           数据对象“属性” 和 标签 “类名” 一致
+           遍历对象，使用属性获取对应标签，快速赋值
+        */
+        const keys = Object.keys(bookObj);
+        // console.log(keys);
+        
+        keys.forEach(key => {
+           document.querySelector(`.edit-form .${key}`).value = bookObj[key];
+        });
+    })
+    editModal.show()
+  }
+})
+
+// // 修改按钮 ，点击，隐藏弹框
+document.querySelector('.edit-btn').addEventListener('click',() => {
+    // 获取表单所有元素
+    const editForm = document.querySelector('.edit-form');
+    // 设置成对象 ,解构不用 .bookObj ，因为对象的属性名和标签的类名一致
+    const {id,bookname,author,publisher} = serialize(editForm,{hash:true,empty:true});
+    // console.log(bookObj);
+    
+    // 编辑key遍历赋值了id
+    /*  保存正在修改的id，并且隐藏
+       <input type="hidden" class="id" >
+    */
+    // 隐藏弹框
+    editModal.hide();
+    // 4.3 提交保存修改，并且刷新列表
+   
+   axios({
+      url: `http://hmajax.itheima.net/api/books/${id}`,
+      method: 'PUT',
+      data: {
+        bookname,
+        author,
+        publisher,
+        creator
+      }
+   }).then( () => {
+      getBooksList();
+      editModal.hide();
+   })
+
+    
 })
--- Ajax/图书管理/02_图书增删改查/readme.md
+++ Ajax/图书管理/02_图书增删改查/readme.md
@@ -24,3 +24,23 @@
         })
       </script>
 ```
+```js
+// target 代表dom（文档对象模型）元素，del是需要检查的类名
+/*  - classList 能根据动态的列表，自动更新，返回指定元素所有类名
+    -  contains 是DOMTokenList对象一个方法，检测列表是否包含制定类名。以字符串为参数，返回ture or false
+    适用场景：判断按钮是否是指定按钮
+*/
+target.classList.contains('del')
+/*  -  注意：url使用模版字符串，`` ,其中 ${theId} 是变量，
+    -  method 指定http请求方法，使用delete方法，从服务器删除资源
+    -  .then() 用于请求成功后回调，promise对象的一个方法
+*/
+axios({
+            url:`http://hmajax.itheima.net/api/books/${theId}`,
+            method:'DELETE',
+        }).then(() => {
+
+            getBooksList(); 
+        })
+
+```
